a114f666c8bee63b8ca7cd2ff18447573ec12785
shna231 <71744479+shna231@users.noreply.github.com>
2020-09-25 17:32:04 +0900

    Update free_auto_dem.sh

--- free_auto_dem.sh
+++ free_auto_dem.sh
@@ -3,24 +3,34 @@
 
 # 한번에 돌릴 터미널 개수 (일단 5개, CPU 스펙보고 차후 변경)
 count=5
-i=0
+i=1
 
-# CPU 사용률 변수: 사용률이 40%를 넘으면 반복문을 쉼.
-
-# 해당 디렉터리 내 파일명 텍스트 파일로 저장.
-ls > dem.txt
+# 해당 디렉터리 내 파일명 텍스트 파일로 저장(missing 데이터 제외).
+ls | grep 'missing' -v > dem.txt
 
 # '파일명 읽기 > 터미널 실행 > 루틴.sh (파일명)' count번씩 반복 실행
 for name in 'cat dem.txt' 
 do
     gnome-terminal -- "./dem_routine.sh ${name}"
     
+    # 5번 실행마다 평균 소요시간(6h)만큼 sleep
     if[5 -eq $i]; then
+      sleep 6h
       
+      # recon-all 프로세스가 존재하는지 10분 단위로 확인
+      while: 
+      do
+        if pgrep -x "recon-all" > /dev/null
+        then
+            sleep 10m
+        else
+            break
+        fi  
+      done
       
-      
-      
-      i = 0
+      # recon all  프로세스 종료 확인 후 i 초기화 > 터미널 작업 반복
+      i=0
+
     fi
     
     i++
