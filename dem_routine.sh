e2467ccffde40ddc71db18eca05570274023c193
shna231 <71744479+shna231@users.noreply.github.com>
2020-09-28 14:12:25 +0900

    Update dem_routine.sh

--- dem_routine.sh
+++ dem_routine.sh
@@ -8,6 +8,7 @@ cd $DATA_PATH/Dementia
 export SUBJECTS_DIR=$DATA_PATH/Dementia
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
+# 확장자명 제외 subj 번호만 가져오기.
 subj="${1%.nii.gz}"
-econ -all $1 -s $subj -all
+econ-all -i $1 -s $subj -all
 
