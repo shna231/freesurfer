b15c604a6df4b8393304bd7d11f11840a613c9c8
shna231 <71744479+shna231@users.noreply.github.com>
2020-09-28 13:54:40 +0900

    Update dem_routine.sh

--- dem_routine.sh
+++ dem_routine.sh
@@ -3,12 +3,11 @@
 
 source /etc/profile.d/freesurfer.sh
 
-
 cd $DATA_PATH/Dementia
 
 export SUBJECTS_DIR=$DATA_PATH/Dementia
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-subj="${1%%.nii.gz}"
+subj="${1%.nii.gz}"
 econ -all $1 -s $subj -all
 
